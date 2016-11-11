f9f911c052ed196e146a3915a6a9a763596e7801
David J. Frailey <d.j.frailey@gmail.com>
2016-11-10 22:35:29 -0800

    Started working towards mysql integration

---
+++ src/Model/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Zephyr\Model\User;
+
+use function Zephyr\Helpers\connectionPool;
+use function Zephyr\Model\Account\createUserAccount;
+use Amp\{Deferred, Promise, Failure, Success, function resolve};
+use \stdClass;
+use \Exception;
+
+function createUser(array $data)
+{
+    static $statement;
+    $result = false;
+
+    if (isset($data['email_address']) === false) {
+        throw new Exception("Email address must be provided when creating a new user.");
+    } else if (isset($data['username']) === false) {
+        throw new Exception("Username must be provided when creating a new user.");
+    } else if (isset($data['name']) === false) {
+        throw new Exception("Name must be provided when creating a new user.");
+    } else {
+
+        if (isset($statement) === false) {
+            $statement = yield connectionPool()->prepare("INSERT INTO users (email_address, username, name) VALUES (:email_address, :username, :name)");
+        }
+
+        yield $statement->execute($data);
+
+        if (isset($data['accounts']) === false) {
+            foreach($data['accounts'] as $account) {
+                $account['user_email_address'] = $data['email_address'];
+                yield resolve(createUserAccount($account));
+            }
+        }
+
+        unset($data['accounts']);
+        $result = $data;
+    }
+
+    return $result;
+}
+
+function getUserByEmail(string $email)
+{
+    static $statement;
+
+    if (isset($statement) === false) {
+        $statement = yield connectionPool()->prepare("SELECT * FROM users WHERE email_address=? LIMIT 1");
+    }
+
+    $set = yield $statement->execute([$email]);
+    $row = yield $set->fetchObject();
+
+    return $row;
+}
+
+function getUserByUsername(string $username)
+{
+    static $statement;
+
+    if (isset($statement) === false) {
+        $statement = yield connectionPool()->prepare("SELECT * FROM users WHERE username=? LIMIT 1");
+    }
+
+    $set = yield $statement->execute([$username]);
+    $row = yield $set->fetchObject();
+
+    return $row;
+}
+
+function getUserByName(string $name)
+{
+    static $statement;
+
+    if (isset($statement) === false) {
+        $statement = yield connectionPool()->prepare("SELECT * FROM users WHERE name=? LIMIT 1");
+    }
+
+    $set = yield $statement->execute([$name]);
+    $row = yield $set->fetchObject();
+
+    return $row;
+}
--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,11 @@ namespace Zephyr\Helpers;
 use Aerys\Request;
 use Amp\Deferred;
 use Amp\Success;
+use Amp\Mysql\Pool;
+
+/*
+    UTILITY
+*/
 
 function getExecutionTime($reset = false)
 {
@@ -60,4 +65,26 @@ function arrayGet(array $data, ...$keys)
     }
     
     return $found;
+}
+
+function env($key = null, $default = null)
+{
+    return $_ENV[$key] ?? $default;
+}
+
+function connectionPool()
+{
+    static $pool;
+
+    if (isset($pool) === false) {
+
+        $host = env("MYSQL_HOST");
+        $user = env("MYSQL_USER");
+        $password = env("MYSQL_PASSWORD");
+        $database = env("MYSQL_DATABASE");
+
+        $pool = new Pool("host=$host;user=$user;pass=\"$password\";db=$database");
+    }
+
+    return $pool;
 }
